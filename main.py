42052b786447a8f60cbeb8ec781c0d96472dc7e9
amenkes319 <amenkes319@gmail.com>
2023-03-02 11:56:02 -0500

    python setup

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from configparser import ConfigParser
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+def main():
+    login = {"username": " ", "password": " "}
+    parser = ConfigParser()
+    parser.read('ssh_login.ini')
+    params = parser.items('login')
+    dbName = "p320_16"
+
+    for param in params:
+        login[param[0]] = param[1]
+    username = login.get("username")
+    password = login.get("password")
+
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('localhost', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
+
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+            print("Database connection established")
+            curs.execute("SELECT * FROM account")
+            print(curs.fetchall())
+    except:
+        print("Connection failed")
+    finally:
+        conn.close()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
